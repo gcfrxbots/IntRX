68f8c519537fb01387b4f14759e730102c0dfdb2
Grant Fowler <gcf.rxbots@gmail.com>
2019-11-30 11:35:23 -0500

    Fix Install_Reqs

--- Setup/Install_Requirements.bat
+++ Setup/Install_Requirements.bat
@@ -1,11 +1,15 @@
-msg %username% You need to have Python 3.7 (and the included PIP package) installed for this to work! Click OK to install all required packages.
+START CMD /C "ECHO You need to have Python 3.7 (and the included PIP package) installed for this to work! Press any key to install all required packages. && PAUSE"
 pause
 
 
+
 py -3.7 -m pip install -r requirements.txt --user --no-warn-script-location
+
 
 cd ../IntRX
 
 py -3.7 ../IntRX/Initialize.py --g
 
+
 pause
+
